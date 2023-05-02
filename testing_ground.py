1a86e75fd5258ef134c3cef4c3f631df0d8438b6
Aditya <mrknightkhonshu34@gmail.com>
2023-05-02 20:05:20 +0530

    Update testing_ground.py

--- testing_ground.py
+++ testing_ground.py
@@ -8,16 +8,23 @@ soup = BeautifulSoup(response.content, "html.parser")
 ending_point = int(soup.find_all("a", class_="page")[-1].text)
 
 
-# all_tags = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+# all_tags = ["num","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 
 sections_div = soup.find("div", class_="container").find_all("section")
 
-# for section in sections_div:
-#     for tag in section:
-#         tag.h2.text
-#         info = 
-#         print()
-#         print("\n")
+for section in sections_div:
+    info = {
+        
+    }
 
-for i in sections_div:
-    print(i["id"])
+# for page in range(ending_point):
+#     num = page + 1
+#     print(num)
+#     print("-------------------------")
+#     url_page = url + "?page=" + str(num)
+#     response = requests.get(url_page)
+#     soup = BeautifulSoup(response.content, "html.parser")
+
+#     sections_div = soup.find("div", class_="container").find_all("section")
+#     print(sections_div)
+#     print("\n")
