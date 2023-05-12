1224c593c4e1d189bcf30cd49f507d2e3361412b
Aditya <mrknightkhonshu34@gmail.com>
2023-05-12 14:15:26 +0530

    Updated tags

--- tags.py
+++ tags.py
@@ -6,13 +6,28 @@ r = requests.get(url)
 
 soup = BeautifulSoup(r.content, "html.parser")
 
-#! Alphabetical Pagination
-# atoz_pagination = [] 
-# pagination = soup.find("ul", class_="alphabetical-pagination").find_all("li")
+pages_limit = soup.findAll("a", class_="page")
+pages_limit = int(pages_limit[-1].text)
 
-# for i in pagination:
-#     atoz_pagination.append(url + (i.a["href"]))
+all_pages_links = []
+for i in range(pages_limit+1):
+    if i == 0:
+        pass
+    else:
+        link = f"{url}?page={i}"
+        all_pages_links.append(link)
 
-#! Tags
-tags_list = []
-tags = soup.find() 
+all_tags_data = []
+for page_link in all_pages_links:
+    i = requests.get(page_link).text
+    s = BeautifulSoup(i, "html.parser")
+
+    links = s.find_all("a", class_="tag")
+
+    for link in links:
+        info = {
+            "a": link["href"],
+            "text": (link.span.text).strip(),
+            "count": link.find("span", class_="count").text
+        }
+        all_tags_data.append(info)
